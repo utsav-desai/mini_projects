09d8758f86c496bbcb7ae502cb7e415892ad989d
Utsav Desai <utsavmdesai@gmail.com>
2023-12-22 12:00:13 +0530

    Edited the FlowNetC model

--- Deep Learning/FlowNetC/model.py
+++ Deep Learning/FlowNetC/model.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
 from utils import conv, deconv, predict_flow, crop_like
+from torch.nn.init import kaiming_normal_, constant_
 
 
 class FlowNetC(nn.Module):
@@ -34,4 +35,8 @@ class FlowNetC(nn.Module):
     self.predict_flow3 = predict_flow(386)
     self.predict_flow2 = predict_flow(194)
 
+    self.upsampled_flow6_to_5 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias = False)
+    self.upsampled_flow5_to_4 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias = False)
+    self.upsampled_flow4_to_3 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias = False)
+    self.upsampled_flow3_to_2 = nn.ConvTranspose2d(2, 2, 4, 2, 1, bias = False)
     
